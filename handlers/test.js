b9d2b199b0130d6fb73f3345bb38867ec0545dc4
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-10-28 10:42:03 +0200

    Adds testpage for testdata

--- handlers/test.js
+++ handlers/test.js
@@ -1,5 +1,25 @@
 'use strict'
 
+const config = require('../config')
+const pkg = require('../package.json')
+
+module.exports.showTestPage = (request, reply) => {
+  const viewOptions = {
+    version: pkg.version,
+    versionName: pkg.louie.versionName,
+    versionVideoUrl: pkg.louie.versionVideoUrl,
+    systemName: pkg.louie.systemName,
+    githubUrl: pkg.repository.url,
+    logoutUrl: config.AUTH_LOGOUT_URL
+  }
+
+  reply.view('test', viewOptions)
+}
+
+module.exports.setupTestData = (request, reply) => {
+  reply.redirect('/')
+}
+
 module.exports.testPing = (request, reply) => {
   request.seneca.act('role: test, cmd: ping', (error, msg) => {
     reply(error || msg)
--- routes/test.js
+++ routes/test.js
@@ -5,6 +5,26 @@ const handler = require('../handlers/test')
 module.exports = [
   {
     method: 'GET',
+    path: '/test',
+    handler: handler.showTestPage,
+    config: {
+      auth: false,
+      security: true,
+      description: 'Show testpage'
+    }
+  },
+  {
+    method: 'POST',
+    path: '/test',
+    handler: handler.setupTestData,
+    config: {
+      auth: false,
+      security: true,
+      description: 'Setup testdata'
+    }
+  },
+  {
+    method: 'GET',
     path: '/test/ping',
     handler: handler.testPing,
     config: {
