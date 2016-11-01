ee78d473e4b8a63d459522a51aa187568710c312
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-11-01 10:16:55 +0100

    Updates show-tiltak

--- views/helpers/show-tiltak.js
+++ views/helpers/show-tiltak.js
@@ -5,6 +5,19 @@ module.exports = (document) => {
   var output = []
 
   output.push(`Navn: ${tiltak.navn}`)
+
+  if (document.formal && document.formal.formal) {
+    output.push(`Formål: ${document.formal.formal}`)
+  }
+
+  if (document.target && document.target.target) {
+    if (Array.isArray(document.target.target)) {
+      output.push(`Målgrupper: ${document.target.target.join(', ')}`)
+    } else {
+      output.push(`Målgruppe: ${document.target.target}`)
+    }
+  }
+
   output.push(`Beskrivelse: ${tiltak.beskrivelse}`)
   output.push(`Periode: ${tiltak.startdato} - ${tiltak.sluttdato}`)
   output.push(`Frekvens: ${tiltak.antall}`)
