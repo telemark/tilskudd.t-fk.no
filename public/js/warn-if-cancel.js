40f4ea6802e210261d66fc26a59244030273f2cd
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-11-29 08:46:58 +0100

    Updates warn if cancel

--- public/js/warn-if-cancel.js
+++ public/js/warn-if-cancel.js
@@ -1,13 +1,25 @@
 'use strict'
 
-var cancelButton = document.getElementById('cancelButton')
+function init () {
+  var cancelButton = document.getElementById('cancelButton')
 
-cancelButton.addEventListener('click', function (e) {
-  e.preventDefault()
+  cancelButton.addEventListener('click', function (e) {
+    e.preventDefault()
 
-  var result = confirm('Er du sikker på at du vil avbryte søknaden? Alt du har gjort vil bli slettet.')
+    var result = confirm('Er du sikker på at du vil avbryte søknaden? Alt du har gjort vil bli slettet.')
 
-  if (result) {
-    window.location.href = cancelButton.getAttribute('href')
+    if (result) {
+      window.location.href = cancelButton.getAttribute('href')
+    }
+  })
+}
+
+function ready (fn) {
+  if (document.readyState != 'loading') {
+    fn ()
+  } else {
+    document.addEventListener('DOMContentLoaded', fn)
   }
-})
+}
+
+ready(init)
